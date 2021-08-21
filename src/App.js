ff60301fdd1f62f48ddc035ce375420fb8aa9623
snodirshox99 <snodirshox99@mail.ru>
2021-08-21 20:39:22 +0500

    login screen WIP

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="leftSide">
+      </div>
+      <div className="rightSide">
+        <div className="rightInner">
+          <h1>Welcome</h1>
+          <div className="inputs">
+            <input type="Email" placeholder="Email" />
+            <input type="password" placeholder="Passwor" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
